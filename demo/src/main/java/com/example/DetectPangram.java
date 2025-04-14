10582ba97fd44a4fd56afb638f680ca6fee984d9
mwijuka <mwijukaharoid@gmail.com>
2025-04-14 07:27:28 +0300

    solved

--- demo/src/main/java/com/example/DetectPangram.java
+++ demo/src/main/java/com/example/DetectPangram.java
@@ -15,31 +15,27 @@ import java.util.regex.Pattern;
  //Algorithm
  /**
   * 1. turn string to array for easy looping
-  * 2. set target / items to search for ie A-Z in this case should return boolean
+  * 2. set target / items to search for ie A-Z 
   * set count to be equal to count of alphabet  = 26
-  * 3. loop through sentense array trying to find target return true if target found
-  * 4. return false f target not found 
+  * 3. loop through sentense array trying to find target return true if target found then increase count
+  * 4. return false if !count == 26  
   */
 public class DetectPangram {
-    // // public boolean check(String sentence){
-         
-    // }
-
+   
     public static void main(String[] args) {
-         String sentence = "The quick brown fox jumps over the lazy dog";
-        String[] sentenceArray = sentence.chars()
-                .mapToObj(c -> String.valueOf((char) c))
-                .toArray(String[]::new);
-        boolean[] checkAlphabet = new boolean[26];
+        String sentence = "Cwm fjord bank glyphs vext quiz";
+        boolean checkAlphabet = false;
         sentence.toLowerCase();
         
         int count = 0;
         for (char i = 'a'; i <= 'z' ; i++) {
             if (sentence.contains(String.valueOf(i))) {
                 count++;
-        
             }
-        }  
-        System.out.println(count);
+        }
+        if (count == 26) {
+            checkAlphabet = true;
+        }
+        System.out.println(checkAlphabet);
     }
 }
